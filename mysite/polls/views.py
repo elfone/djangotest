4df3a2457bcbbd4f0c02be06818333ead0c2a0dd
elfone <elfone@yeah.net>
2015-04-16 13:48:28 +0800

    编写你的第一个 Django 程序 第3部分

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Poll
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
 
 
 def detail(request, poll_id):
